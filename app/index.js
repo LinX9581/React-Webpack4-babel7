4960d960428d41fc73e2cd47d8cc4a7c42c984ae
linx <linx9581@gmail.com>
2019-01-01 23:33:43 +0800

    basic router

--- app/index.js
+++ app/index.js
@@ -1,18 +1,30 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-require('./index.css');
+var React = require("react");
+var ReactDOM = require("react-dom");
+require("./index.css");
+import Home from "../components/home";
+import About from "../components/about";
+import Contact from "../components/contact";
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>
-                asdf
-            </div>
-        )
-    }
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+            <ul>
+                <li><Link to='/about'>about</Link></li>
+                <li><Link to='/contact'>contact</Link></li>
+                <li><Link to='/home'>home</Link></li>
+            </ul>
+          <Route path='/about' component={About}/>
+          <Route path='/contact' component={Contact}/>
+          <Route path='/home' component={Home}/>
+        </div>
+      </Router>
+    );
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));
